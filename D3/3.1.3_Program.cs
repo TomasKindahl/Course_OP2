eef36264aac8aa4f258124b295ea5871fba84cca
Tomas Kindahl <tomas.kindahl@bahnhof.se>
2024-04-11 21:10:14 +0200

    lösning till 3.1.3

---
+++ D3/3.1.3_Program.cs
@@ -0,0 +1,34 @@
+namespace ovn_3_1_3
+{
+    internal class Program
+    {
+        struct Point2D
+        {
+            public double X, Y;
+            public Point2D(double x, double y) { X = x; Y = y; }
+            public override string ToString()
+                => $"({X} {Y})";
+            static public Point2D operator +(Point2D p1, Point2D p2)
+                => new Point2D(p1.X+p2.X, p1.Y+p2.Y);
+            static public Point2D operator -(Point2D p)
+                => new Point2D(-p.X, -p.Y);
+            static public Point2D operator -(Point2D p1, Point2D p2)
+                => p1 + -p2;
+            public double Distance(Point2D other)
+            { 
+                double dX = this.X - other.X;
+                double dY = this.Y - other.Y;
+                return Math.Sqrt(dX*dX + dY*dY);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Point2D P1 = new Point2D(2, -1);
+            Point2D P2 = new Point2D(3, 2);
+            Console.WriteLine($"{P1} + {P2} =  {P1 + P2}");
+            Console.WriteLine($"-{P1} = {-P1}");
+            Console.WriteLine($"{P1} - {P2} =  {P1 - P2}");
+            Console.WriteLine($"{P1}.Distance({P2}) = {P1.Distance(P2)}");
+        }
+    }
+}
